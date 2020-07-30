aae1392971aa663cb46f75122c0ddf05657e0db9
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-07-30 16:13:07 +0100

    added hacky non-working code

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,8 +1,12 @@
 def roman_numerals(num)
-  case
-  when num == 1
-    "I"
-  when num == 5
-    "V"
-  end
+ values = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 100 => "C"}
+ letters = ""
+ values =
+
+ values.each do |value, letter|
+   letters << letter * (num / value)
+   num = num % value
+ end
+ puts letters
 end
+roman_numerals(326)
