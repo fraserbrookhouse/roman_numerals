5f5ad8cc40b2f7c98165c6377ca0cb2e0f485e03
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-07-30 15:40:57 +0100

    created lib dir with roman_numerals.rb inside

--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -1,7 +1,10 @@
 require 'roman_numerals'
 
 describe 'roman_numerals' do
-  it 'returns I when passed 1' do
+  it 'returns "I" when passed 1' do
     expect(roman_numerals(1)).to eq 'I'
+  end
+  it 'returns "V" when passed 5' do
+    expect(roman_numerals(5)).to eq 'V'
   end
 end
