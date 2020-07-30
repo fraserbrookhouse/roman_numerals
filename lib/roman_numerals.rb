50314f40dedc44eee4665c926e98e0fcd5a65c34
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-07-30 15:46:29 +0100

    added third test, passed second test

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,8 @@
+def roman_numerals(num)
+  case
+  when num == 1
+    "I"
+  when num == 5
+    "V"
+  end
+end
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -7,4 +7,7 @@ describe 'roman_numerals' do
   it 'returns "V" when passed 5' do
     expect(roman_numerals(5)).to eq 'V'
   end
+  it 'returns "CCCXXVI" when passed 326' do
+    expect(roman_numerals(326)).to eq 'CCCXXVI'
+  end
 end
