67a763ef383fb451fcc8c8aa1327f166d56bdeed
Fraser Brookhouse <fbrookhouse@gmail.com>
2020-07-30 17:02:37 +0100

    added value for L

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,13 +1,12 @@
 def roman_numerals(num)
 
- values = { 100 => "C", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
- 
+ values = { 100 => "C", 50 => "L", 10 => "X", 9 => "IX", 5 => "V", 4 => "IV", 1 => "I" }
+
  letters = ""
- 
+
  values.each do |value, letter|
    letters << letter * (num / value)
    num = num % value
   end
   return letters
 end
-
